b29f59a1b72ee22c3b2ed83160e95014137c64e2
BeigeBear <cameogu.work@gmail.com>
2022-09-23 17:50:21 +0900

    0923 1550

---
+++ EduExample_0923/src/EngineOil/EngineOil.java
@@ -0,0 +1,25 @@
+package EngineOil;
+
+import java.lang.invoke.SwitchPoint;
+
+public class Car{
+	// Field
+	private String carModel;
+	
+	// Constructor
+	public Car(String carmodel) {
+	}
+}
+	
+public class changeEngineOil {
+	Car c;
+	
+	public changeEngineOil();
+	
+	
+	public void changeEngineOil(Car c) {
+		c.changeEngineOil();
+	}
+	
+
+}
